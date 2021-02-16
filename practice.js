f78f46dd3100b855c55b070e7681697125e88731
nikki-ai <shony1198@gmail.com>
2021-02-16 00:23:23 -0600

    started #1

--- practice.js
+++ practice.js
@@ -28,4 +28,64 @@ class LinkedList {
       tempNode.next = new _Node(item, null);
     }
   }
+  find(item) {
+    //Start at head
+    let currNode = this.head;
+    //If list is empty
+    if (!this.head) {
+      return null;
+    }
+    //Check for item
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+    return currNode;
+  }
+  remove(item) {
+    //If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // If the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    //Start at the head
+    let currNode = this.head;
+    //Keep track of prev.
+    let previousNode = this.head;
+
+    while (currNode !== null && currNode.value !== item) {
+      // Save the prev. node
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
 }
+
+function main() {
+  let SLL = new LinkedList();
+
+  SLL.insertFirst('Apollo');
+  SLL.insertFirst('Boomer');
+  SLL.insertFirst('Helo');
+  SLL.insertFirst('Husker');
+  SLL.insertFirst('Starbuck');
+  SLL.insertFirst('Tauhida');
+  SLL.remove('squirrel');
+  
+  
+  return SLL;
+}
+
+console.log(main());
